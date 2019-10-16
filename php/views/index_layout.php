1afc2a3887466bfe0ca08ff61373d6ad784b67dd
Xzevs <rasmus.karlsson@elev.ga.ntig.se>
2019-10-16 11:17:31 +0200

    Gjorde css

--- php/views/index_layout.php
+++ php/views/index_layout.php
@@ -6,6 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= $pageTitle; ?></title>
     <link rel="stylesheet" href="css/style.css">
+    <style type="text/css">
+<!--
+a:link { color: #000000; text-decoration: none}
+-->
+</style>
 </head>
 <body>
     <main>
